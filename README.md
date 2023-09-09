e8dfd1dc7adc6deb3c3432e79a069944cb5a3620
byjosh <52639095+byjosh@users.noreply.github.com>
2023-09-10 00:53:04 +0100

    Update index Python file and README

--- README.md
+++ README.md
@@ -7,39 +7,92 @@ If you don't run `git clone https://github.com/byjosh/index_formatter.git` comma
 
 Put your input CSV file (called `test_book_before_page.csv` or `test_page_before_book.csv` in examples below depending on whether input file has book before page number column or vice versa) in same folder as the index_to_html.py script (so where you unzipped the files - or cloned them if you did a git clone).
 
-index_to_html.py is the script to run in the terminal or Powershell
+index_to_html.py is the script to run in the terminal or Powershell. The default input CSV column order is Entry, Book, Page, Notes, Tags (see **Input file column order** section below)
 
 Command to run in Mac/Linux terminal is like: 
 
-`python3 index_to_html.py test_book_before_page.csv output_file_title`
+`python3 index_to_html.py test_book_before_page.csv -o "output_file_title"`
 
 or on Windows in Powershell the command is like: 
 
-`python3.exe .\index_to_html.py .\test_book_before_page.csv output_file_title`
+`python3.exe .\index_to_html.py .\test_book_before_page.csv -o "output_file_title"`
 
 Change `test_book_before_page.csv` and  `output_file_title` to appropriate values (though leave as is and it will test the script with the provided test file: test_book_before_page.csv.
 
-## Input file requirements
+`-c` outputs a CSV and another flag `-t` processes the tags so each tag in tag column is prefixed to the main index term to create a new entry.
 
-The column layout in the CSV for using it simply as`python3 index_to_html.py test_book_before_page.csv output_file_title` or `python3.exe .\index_to_html.py .\test_book_before_page.csv output_file_title` should be book number then page number:-
+On Windows in Powershell the command is like: 
+
+`python3.exe .\index_to_html.py .\test_book_before_page.csv -o "output_file_title" -c -t`
+
+It comes with built in help - so experiment.
+
+## Builtin help
+
+Any options that have `-o OUTPUTTITLE` format are best written quotes if there are any characters in string that could mess with terminal (the capitalized OUTPUTTITLE indicates you have to supply a string if using the option) e.g. `-o "Today's index test"`. 
+
+`-f {ebpnt,ebcpnt,enbcpt,enbpt,bpent,bcpent}` also needs input after the `-f` option e.g. `-f ebcpnt`.
+
+
+
+> usage: index_formatter.py [-h] [-o OUTPUTTITLE] [-f {ebpnt,ebcpnt,enbcpt,enbpt,bpent,bcpent}] [-t] [-ts TAGSEPARATOR] [-c] [-os OUTPUTSEPARATOR] [-v] [-vv] inputCSVfile\
+>takes a CSV file with columns for entry, book #, [chapter#] , page#, description and tags fields (not necessarily in that order and outputs order HTML or a CSV with tags prepended to the entry\
+positional arguments:\
+  inputCSVfile          input CSV file to be processed - see README and help below re: order of columns needed\
+options:\
+  -h, --help            show this help message and exit\
+> 
+> -o OUTPUTTITLE, --outputtitle OUTPUTTITLE\
+                        title used in naming output document and in HTML head\
+ > \
+> -f {ebpnt,ebcpnt,enbcpt,enbpt,bpent,bcpent}, --fields {ebpnt,ebcpnt,enbcpt,enbpt,bpent,bcpent}\
+                        specify order of columns (fields) in your CSV file where each letter is initial letter of following (might call your columns slightly differently but this make clear function of each column): Entry      
+                        (what you look up in index e.g. apple), Book (or volume e.g. vol 1) , Chapter [optional], Notes (notes or description on Entry item e.g. popular fruit), Tags [optional in sense program will check if     
+                        this is empty column] - comma separated list of classifications that might apply to the entry e.g. for apple depending on context: fruit, tree, cooking, literacy, physics, Abrahamic religion (those are  
+                        just examples - in botanical context just fruit and tree would be relevant)\
+> \
+> -t, --tags            process tags so that entries are added with the tag prepended to the entry - using tag separator (defaults to : )\
+>\
+> -ts TAGSEPARATOR, --tagseparator TAGSEPARATOR\
+                        The separator between a tag and an entry when adding an entry of tag tagseparator entry default set to : - likely a colon followed by a space \
+> 
+> -c, --csv             output file as CSV - if used in conjunctions\ 
+-os OUTPUTSEPARATOR, --outputseparator OUTPUTSEPARATOR\
+                        when outputting HTML the book, chapter, page numbers will be concatenated and joined this seperator is between them\
+> 
+>  -v                    logging.INFO level of log verbosity\
+> 
+>   -vv                   logging.DEBUG level of logging verbosity
+
+
+
+
+
+## Input file column order
+
+The input file column order by default is `ebpnt`.
+* Entry (e) - what you look up in an index
+* Book (b) - the book or volume it is found in
+* Chapter (c) - optional chapter
+* Notes (n) - the longer notes or description regarding an index entry
+* Tags (t) - a comma separated list of tags
+
+Possible orders are `ebpnt`, `epbnt`, `ebcpnt`,`enbcpt`,`enbpt`,`bpent`,`bcpent`,`entbp`,`entbcp` (see `field_orders` list in code).
+### Example input column orders
 ```
-entry,  book, page, notes
-"apple","Book 1", 5,  "often mentioned when teaching letter A in English alphabet"
+entry,  book, page, notes, tags
+"apple","Book 1", 5,  "often mentioned when teaching letter A in English alphabet","fruit,tree,cooking"
 ```
-### Input with page number column before book column
-Run with `page` as a final argument as `python3 index_to_html.py test_page_before_book.csv output_file_title  page` or `python3.exe .\index_to_html.py .\test_page_before_book.csv output_file_title  page` the column layout in the CSV can be entry (e.g. "apple"), page number, book number (e.g. Book 1), notes (e.g. "often mentioned in connection with teaching the alphabet in English")
-
-
-
+The above seen in `test_book_before_page.csv` would be specified `-f=ebpnt` on the command line - but is the default anyway
+The below seeen in `test_page_before_book.csv` would be specified `-f=epbnt` on the command line.
 ```
-entry, page, book, notes
-"apple", 5, "Book 1", "often mentioned when teaching letter A in English alphabet"
+entry, page, book, notes, tags
+"apple", 5, "Book 1", "often mentioned when teaching letter A in English alphabet","education, cooking"
 ```
-
+## Misc notes
 Remove any row of column labels before using the script (or modify script to have that as an option). Any lines without an entry in first column are ignored for output
 
 The order seen for the page numbers for each entry will work best if all book references end so last character is a number (whether `book 3`, `volume 3`, `101.3`) and the page number is a number with no other characters (so: `100` ; not `100-102`) - but the comparison function uses strings to make the comparison - just in case you don't follow these rules. But unless an entry has a half a dozen or more pages referenced one can probably spot the correct order at a glance even if your input data does not play nice with the comparison function.
 
-### Test files
-`test_book_before_page.csv` and `test_page_before_book.csv` are provided as test files. With the latter the optional `page` argument is needed as final argument like `python3 index_to_html.py test_page_before_book.csv output_file_title  page` or `python3.exe .\index_to_html.py .\test_page_before_book.csv output_file_title  page` - with `test_book_before_page.csv` that final argument can and indeed should be omitted (as the default column order is `entry,  book, page, notes`).
-##
+## Test files
+`test_book_before_page.csv` and `test_page_before_book.csv` are provided as test files. With the latter the optional `-f=epbnt` argument is needed as final argument like `python3 index_to_html.py test_page_before_book.csv output_file_title  -f=epbnt` or `python3.exe .\index_to_html.py .\test_page_before_book.csv output_file_title  -f=epbnt`.
--- index_to_html.py
+++ index_to_html.py
@@ -13,56 +13,59 @@ import html
 import argparse
 
 # if you want to see the master dictionary created change logging.FATAL to logging.DEBUG
-logging.basicConfig(level=logging.DEBUG)
+loglevel = logging.FATAL
 
 """have Location tuple with book and page, then list of tuples consisting of (book, page num, entry)"""
 Location = namedtuple('Location', 'book page')
 Entry = namedtuple('Entry', 'location notes taglist chapter')
-tagseparator = ": "
-outputseparator = '--'
-csv_input_filename = None
-title = None
-order = 'ebpnt'
-""" The inputs that we want are 
-inputfile [required]
-title [optional]
-field order [optional]
-output [optional ]
-tags [optional]
-
-The default is it takes a CSV, outputs an HTML file, does not add entries based on tags, 
-and has a field order of entry, book, page, notes, tags
-
-possible orders are (grounds for using a switch case perhaps)
-"""
+# possible field orders for columns in CSV: Entry, Book, Chapter, Notes, Tags
 field_orders = ['ebpnt',
+                'epbnt',
                 'ebcpnt',
                 'enbcpt',
                 'enbpt',
                 'bpent',
                 'bcpent',
+                'entbp',
+                'entbcp',
                 ]
-
+# set some defaults
+tagseparator = ": "
+outputseparator = ' -- '
 
 parser = argparse.ArgumentParser(prog="index_as_html.py",
-                                 description="takes a CSV file with columns for entry, book #, [chapter#] , page#, description and tags fields (not necessarily in that order and outputs order HTML or a CSV with tags prepended to the entry")
+                                 description="takes a CSV file with columns for entry, book #, [chapter#] , page#, description and tags fields (not necessarily in that order and outputs order HTML or a CSV with tags prepended to the entry - if your output has weird characters try saving your input as utf-8 (the format for output)")
 
 parser.add_argument('inputCSVfile',
                     help='input CSV file to be processed - see README and help below re: order of columns needed')
 parser.add_argument('-o', '--outputtitle', action='store', help="title used in naming output document and in HTML head")
-parser.add_argument('-f', '--fields', action='store',choices=field_orders,
+parser.add_argument('-f', '--fields', action='store', choices=field_orders,
                     help=f'specify order of columns (fields) in your CSV file where each letter is initial letter of following (might call your columns slightly differently but this make clear function of each column):\nEntry (what you look up in index e.g. apple),\nBook (or volume e.g. vol 1) , \nChapter [optional], Notes (notes or description on Entry item e.g. popular fruit), \nTags [optional in sense program will check if this is empty column] - comma separated list of classifications that might apply to the entry e.g. for apple depending on context: fruit, tree, cooking, literacy, physics, Abrahamic religion (those are just examples - in botanical context just fruit and tree would be relevant)')
 parser.add_argument('-t', '--tags', action='store_true',
                     help=f'process tags so that entries are added with the tag prepended to the entry - using tag separator (defaults to {tagseparator} )')
-parser.add_argument('-ts','--tagseparator',action='store',help=f'The separator between a tag and an entry when adding an entry of tag tagseparator entry default set to { tagseparator} - likely a colon followed by a space')
+parser.add_argument('-ts', '--tagseparator', action='store',
+                    help=f'The separator between a tag and an entry when adding an entry of tag tagseparator entry default set to {tagseparator} - likely a colon followed by a space')
 parser.add_argument('-c', '--csv', action='store_true', help='output file as CSV - if used in conjunctions')
-parser.add_argument('-os','--outputseparator',action='store',help='when outputting HTML the book, chapter, page numbers will be concatenated and joined this seperator is between them')
-
+parser.add_argument('-os', '--outputseparator', action='store',
+                    help='when outputting HTML the book, chapter, page numbers will be concatenated and joined this seperator is between them')
+parser.add_argument('-v', action='store_true', help='logging.INFO level of log verbosity')
+parser.add_argument('-vv', action='store_true', help='logging.DEBUG level of logging verbosity')
 
 
 args = parser.parse_args()
-print(args)
+
 # set various fields away from defaults
+if args.v:
+    loglevel = logging.INFO
+if args.vv:
+    loglevel = logging.DEBUG
+logging.basicConfig(level=loglevel)
+logging.info(f'CSV dialects are {csv.list_dialects()}')
+logging.info(f'arguments passed are {args}')
+
+title = None
+order = 'ebpnt'
+
 csv_input_filename = html.escape(args.inputCSVfile)
 if args.outputtitle:
     title = html.escape(args.outputtitle)
@@ -72,6 +75,33 @@ if args.tagseparator:
     tagseparator = html.escape(args.tagseparator)
 if args.outputseparator:
     outputseparator = html.escape(args.outputseparator)
+logging.debug(f'sys.argv was {sys.argv}')
+logging.info(f'Order is {order} ')
+
+
+# END reset defaults
+# START utility functions
+def output_file_message(outputname):
+    """Utility function for message indicating successful file writing"""
+    print(f'Created {outputname} as a file in the same folder as this script')
+
+
+def tuple_string(entry_tuple):
+    """Function used to get key for ordering book - relies on last char of book sting being a digit"""
+    book_num = entry_tuple.location.book.strip()[-1:]
+    page_num = entry_tuple.location.page
+    if book_num.isdigit() and page_num.isdigit():
+        # so book 4 page 40 is not more than book 6 page 7 use x 1000
+        float_value = (int(book_num) * 1000) + int(page_num)
+        # use a string anyway so results are comparable
+        return str(float_value)
+    elif not book_num.isdigit() or not page_num.isdigit():
+        # it went wrong - use a string
+        print(
+            f'For better ordering have last character of {entry_tuple.location.book} as number not {book_num} and {page_num} as number')
+        return html.escape(f'{book_num}-{page_num}', quote=True)
+
+
 def not_file(file):
     """Test file is missing"""
     if path.isfile(file):
@@ -88,31 +118,12 @@ def not_csv(file):
         return True
 
 
-logging.debug(sys.argv)
-
+# END utility functions
 
 if not_file(csv_input_filename) or not_csv(csv_input_filename):
+    # if the input file does not exist exit
     print("first argument does not exist as file or is not a .csv file")
-    exit()
-
-elif len(sys.argv) == 4 and isinstance(sys.argv[1], str):
-
-    logging.info(sys.argv)
-    logging.info(sys.argv[3])
-    if isinstance(sys.argv[3], str) and sys.argv[3] in ["page", 'book']:
-        order = sys.argv[3]
-
-    else:
-        print('arguments are inputfilename, output title,  page')
-        print('page indicate if the entry, page, book, notes is order rather than entry, book, page, notes')
-        exit()
-
-logging.info(f'Order is {order} first')
-
-
-# Export your index as a comma separated values file (CSV format)
-# have the strings quoted in double quotes like "this"
-# filename - rename this to your CSV file
+    sys.exit()
 
 
 def create_master_dict(csv_input_filename: str) -> dict:  #
@@ -133,6 +144,7 @@ def create_master_dict(csv_input_filename: str) -> dict:  #
         taglist = None
 
         for row in index_reader:
+            logging.info(f'row length is {len(row)}')
             if chapter != -1:
                 chapter_no = row[chapter]
             else:
@@ -143,48 +155,46 @@ def create_master_dict(csv_input_filename: str) -> dict:  #
                 taglist = None
             logging.debug(f'Current row is {row}')
             if row[entry] in master_dict.keys():
-                master_dict[row[entry]].append(Entry(Location(row[book], row[page]), row[notes],taglist,chapter_no))
+                master_dict[row[entry]].append(Entry(Location(row[book], row[page]), row[notes], taglist, chapter_no))
             elif row[entry] not in master_dict.keys() and row[entry].strip() != '':
                 # the row[entry].strip() != '' means ignore line if no entry
-                master_dict[row[entry]] = [Entry(Location(row[book], row[page]), row[notes],taglist,chapter_no)]
-            if args.tags is True and taglist is not None:
+                master_dict[row[entry]] = [Entry(Location(row[book], row[page]), row[notes], taglist, chapter_no)]
+            if args.tags is True and taglist != '' and taglist is not None:
                 for current_tag in taglist.split(','):
                     current_tag = current_tag.strip()
                     current_tag_entry = f'{current_tag}{tagseparator}{row[entry]}'
                     if current_tag_entry in master_dict.keys():
+                        # TODO: this assumes another entry with same tag may exist - valid assumption?
                         master_dict[current_tag_entry].append(
-                                Entry(Location(row[book], row[page]), row[notes], taglist, chapter_no))
+                            Entry(Location(row[book], row[page]), row[notes], taglist, chapter_no))
                     elif current_tag_entry not in master_dict.keys() and row[entry].strip() != '':
                         # the row[entry].strip() != '' means ignore line if no entry
-                        master_dict[current_tag_entry] = [Entry(Location(row[book], row[page]), row[notes], taglist, chapter_no)]
+                        master_dict[current_tag_entry] = [
+                            Entry(Location(row[book], row[page]), row[notes], taglist, chapter_no)]
         logging.debug(f'dictionary of entries generated is {master_dict}')
+        list_keys = list(master_dict.keys())
+        last = list_keys[-1]
+        midentry = list_keys[int(len(list_keys) / 2)]
+        logging.info(f'midpoint sample is {master_dict[midentry]}')
+        logging.info(f'endpoint sample is {master_dict[last]}')
     return master_dict
 
 
-def html_item_output(item):
-    return f'<div class="moredetails"><span class="location">{html.escape(item.location.book, quote=True)} -- {html.escape(item.location.page, quote=True)}</span>\
-    <span class="notes">{html.escape(item.notes, quote=True)}</span></div>'
-
-
 def create_page_html():
+    """Main function creating HTML for output"""
+
+    def html_item_output(item):
+        """Creates a div for each Entry tuple per index entry (dict key in master_dict"""
+        if not item.chapter:
+            chapter = ''
+        else:
+            chapter = item.chapter + outputseparator
+        return f'<div class="moredetails"><span class="location">{html.escape(item.location.book, quote=True)}{html.escape(outputseparator)}{html.escape(chapter)}{html.escape(item.location.page, quote=True)}</span>\
+        <span class="notes">{html.escape(item.notes, quote=True)}</span></div>'
+
     entries = create_master_dict(csv_input_filename)
     sorted_keys = sorted(entries.keys(), key=str.casefold)
     output = html_page_components.get_html_header(title)
-
-    def tuple_string(entry_tuple):
-        """Function used to get key for ordering book - relies on last char of book sting being a digit"""
-        book_num = entry_tuple.location.book.strip()[-1:]
-        page_num = entry_tuple.location.page
-        if book_num.isdigit() and page_num.isdigit():
-            # so book 4 page 40 is not more than book 6 page 7 use x 1000
-            float_value = (int(book_num) * 1000) + int(page_num)
-            # use a string anyway so results are comparable
-            return str(float_value)
-        elif not book_num.isdigit() or not page_num.isdigit():
-            # it went wrong - use a string
-            print(
-                f'For better ordering have last character of {entry_tuple.location.book} as number not {book_num} and {page_num} as number')
-            return html.escape(f'{book_num}-{page_num}', quote=True)
 
     for key in sorted_keys:
         list_of_Entry_tuples = sorted(entries[key], key=tuple_string)
@@ -197,16 +207,49 @@ def create_page_html():
     return output
 
 
-def write_page_to_file():
-    # write the html output to a file
-    now = time.strftime('%Y%m%d-%H%M%S')
+def write_html_file(now):
     output_name = f'{title}_as_html_{now}.html'
     with open(output_name, mode='w', encoding='utf-8') as file:
         file.write(create_page_html())
-    print(f'Created {output_name} as a file in the same folder as this script')
-    logging.debug(f'The arguments provided to script were {sys.argv[1:]}')
+    output_file_message(output_name)
+    logging.debug(f'The arguments provided to script were {sys.argv[1:]} as sys.argv and as argparse {args}')
 
 
-if csv_input_filename is not None:
-    #write_page_to_file()
-    create_master_dict(csv_input_filename)
+def write_csv_file(now):
+    output_name = f'{title}_as_csv_{now}.csv'
+    import csv
+
+    with open(output_name, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file, dialect='excel')
+        entries = create_master_dict(csv_input_filename)
+        sorted_keys = sorted(entries.keys(), key=str.casefold)
+        for key in sorted_keys:
+            list_of_Entry_tuples = sorted(entries[key], key=tuple_string)
+            for item in list_of_Entry_tuples:
+                tags = ''
+                if item.taglist:
+                    tags = item.taglist
+                # TODO: tags output to CSV file - so tag: entry is possible but so is tag: tag: tag: entry if one ran the script recursively - desirable or not?
+                if item.chapter:
+                    writer.writerow([key, item.location.book, item.chapter, item.location.page, item.notes,
+                                     tags])
+                if not item.chapter:
+                    writer.writerow([key, item.location.book, item.location.page, item.notes, tags])
+
+    output_file_message(output_name)
+
+
+def write_to_file():
+    """writes output to a file"""
+    # write the output to a file -
+    now = time.strftime('%Y%m%d-%H%M%S')
+    if not args.csv:
+        logging.info("writing HTML file")
+        write_html_file(now)
+    if args.csv:
+        logging.info("writing CSV file")
+        write_csv_file(now)
+
+
+if __name__ == "__main__":
+    write_to_file()
